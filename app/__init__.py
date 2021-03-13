49f99bb45d8066932ae08ffe8d18b404ebe58380
ahojo <ahojodev@gmail.com>
2021-03-13 16:03:50 -0500

    Started flask forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Configuration
+
 app = Flask(__name__)
+app.config.from_object(Configuration)
 
-
-from app import routes  # noqa
+from . import routes  # noqa - This stops autopep8 from pushing it to the top of the file.
+# . means inside this app package.
