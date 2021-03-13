ee997ce3ce67a5bdbdc95f99a8d74c2aee66da7e
ahojo <ahojodev@gmail.com>
2021-03-13 16:54:04 -0500

    Made a login form

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 from app import app
 
 
@@ -17,3 +18,17 @@ def index():
         }
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    """
+    When the browser sends the POST request as a result of the user pressing the submit button, form.validate_on_submit() is going to gather all the data, run all the validators attached to fields, and if everything is all right it will return True, indicating that the data is valid and can be processed by the application. 
+    """
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
